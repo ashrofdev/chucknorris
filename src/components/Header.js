c411a5289c4864179db861b8df7a187fd92471ec
Ashrof Salman <salmanashraf853@gmil.com>
2021-12-10 10:49:47 +0100

    creating components

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header grid">
+            <div className="grid-content">
+                <nav className="header-nav">
+                    <ul className="list-container">
+                        <li className="item">so funktioniert's</li>
+                        <li className="item">sonderangebote</li>
+                        <li className="item">mein bereich</li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
