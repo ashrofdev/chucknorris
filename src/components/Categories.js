1a00013a3a6c0b498210f67702d9287c74552a3a
Ashrof Salman <salmanashraf853@gmil.com>
2021-12-10 11:41:57 +0100

    building categories UI

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -2,8 +2,30 @@ import React from 'react';
 
 const Categories = () => {
     return (
-        <div>
-            categories
+        <div className="grid categories">
+            <div className="grid-content items">
+                <div className="category">
+                    Adilt jokes
+                </div>
+                <div className="category">
+                    Adilt jokes
+                </div>
+                <div className="category">
+                    Adilt jokes
+                </div>
+                <div className="category">
+                    Adilt jokes
+                </div>
+                <div className="category">
+                    Adilt jokes
+                </div>
+                <div className="category">
+                    Adilt jokes
+                </div>
+                <div className="category">
+                    Adilt jokes
+                </div>
+            </div>
         </div>
     );
 };
