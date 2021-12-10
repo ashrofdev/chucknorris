a356ce7aff326c521e66713a753c9e2dd1df664c
Ashrof Salman <salmanashraf853@gmil.com>
2021-12-10 11:27:37 +0100

    building hero UI

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Hero = () => {
     return (
-        <div>
-            hero
+        <div className="hero">
+            
+            <h1 className="heading">The Joke Bible</h1>
+            <h3>Daily laughs for you and yours</h3>
+
+            <div className="filter-section">
+                <input className="input" placeholder="How can we make you laugh today"/>
+            </div>
         </div>
     );
 };
